cef5f21e82e1161a0086ac3a6f24b58d15b222ce
Priyank <priyaaank@gmail.com>
2010-08-25 22:39:49 +0530

    Added src project and test project

---
+++ BPositive/src/com/barefoot/bpositive/Dashboard.java
@@ -0,0 +1,13 @@
+package com.barefoot.bpositive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dashboard extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
