ad86a48a5059f4412edcaba3a7f58c700f6abc5f
Priyank Gupta <priyaaank@gmail.com>
2011-01-20 16:07:55 +0530

    Added ruby/rails webservice app for bpositive

---
+++ bpositive-ws/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BpositiveWs::Application.config.secret_token = '5914bb10791246c480fb173469aec9fd13d447a20b4fb1eb38a9e658536f4b702e8b10f1879725069a8ff32af58d617681b718ee1df95d7ba3d0956eac67c089'
